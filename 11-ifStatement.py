0b06d5364be9540a96cb0904d5b4ac0aa129dd5b
Emre <emre.alkayaa@gmail.com>
2020-09-04 20:05:37 +0300

    Add files via upload
    update

--- 11-ifStatement.py
+++ 11-ifStatement.py
@@ -25,9 +25,8 @@ else:
 a = 10
 b = 15
 c=4
-
-###if a<b or b>c:
-   ###print("emre")
+if a<b or b>c:
+	print("emre")
 
 
 isDead = False
@@ -42,3 +41,33 @@ else:
 
 if not isDead:
 	print("character is not dead")
+
+
+my_string = "emre"
+
+
+if my_string == "emre":
+	print("true")
+else:
+	print("false")
+
+if "emre" in my_string:
+	print("true")
+else:
+	print("false")
+
+
+my_list = [1,2,3,4,5]
+
+if 3 in my_list:
+	print("true")
+else:
+	print("false")
+
+
+my_dictionary = {"k1":100,"k2":200,"k3":300}
+
+if 100 in my_dictionary:
+	print("true")
+else:
+	print("false")
